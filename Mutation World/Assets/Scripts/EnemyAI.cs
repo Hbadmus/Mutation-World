28b7a46199a58cc4507c9d810390ea2477fda507
Hbadmus <Hbadmus1@gmail.com>
2024-04-13 01:14:36 -0400

    Finished zombie functality on lvl1 and most the most part abtracted out the EnemyAI script, Fixed ghost floating problem

--- Mutation World/Assets/Scripts/EnemyAI.cs
+++ Mutation World/Assets/Scripts/EnemyAI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-     public enum FSMStates {
+    public enum FSMStates
+    {
         Idle,
         Chase,
         Attack,
@@ -14,73 +15,76 @@ public class EnemyAI : MonoBehaviour
 
     public FSMStates currentState;
     public float Attackdistance = 4;
-    public int ChaseDis = 50;
     public GameObject throwable;
     public GameObject spawnpoint;
     public Transform player;
-    public float damRate = 2;
+    public int damRate = 2;
     public int damAmt = 10;
-    public int damBossAmt = 10;
-    EnemyHealth enemyHealth;
+    public int timer = 1;
+    public Transform enemyEyes;
+    public int FOV;
+   
+    EnemyHealth enemyHealth; 
     int health;
-
-    Animator anim;
+    NavMeshAgent agent;
     Vector3 nextDestination;
-    int currentDestinationIndex = 0;
     float disToPlayer;
     float elapsedTime = 0;
-    public int timer = 1;
-    Transform deadTransform;
     bool hasAttacked;
     bool isDead;
+    ZombieAI zombieAI;
 
-    // Start is called before the first frame update
     void Start()
     {
-                if(player == null) {
+        if (player == null)
+        {
+            if(gameObject.CompareTag("Boss")) {
+        Debug.Log(player);
+            }
             player = GameObject.FindGameObjectWithTag("Player").transform;
+                   if(gameObject.CompareTag("Boss")) {
+        Debug.Log(player);
+            }
         }
 
         currentState = FSMStates.Idle;
-         enemyHealth = GetComponent<EnemyHealth>();
 
-         health = enemyHealth.currentHealth;
-         isDead = false;
-         anim = GetComponent<Animator>();
-         InvokeRepeating("IncrementTimer", 0f, 1f);
+        enemyHealth = GetComponent<EnemyHealth>();
+        health = enemyHealth.currentHealth;
+        isDead = false;
 
+        agent = GetComponent<NavMeshAgent>();
+        zombieAI = GetComponent<ZombieAI>();
 
+        InvokeRepeating("IncrementTimer", 0f, 1f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-    
         health = enemyHealth.currentHealth;
         disToPlayer = Vector3.Distance(transform.position, player.transform.position);
-    if (transform.rotation.eulerAngles.x != 0 || transform.rotation.eulerAngles.z != 0) {
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-    }
 
-        switch(currentState){
-         case FSMStates.Idle:
-            UpdateIdleState();
-            break;
-         case FSMStates.Chase:
-            UpdateChaseState();
-            break;
-        case FSMStates.Attack:
-            UpdateAttackState();
-            break;
-         case FSMStates.Dead:
-            UpdateDeadState();
-            break;   
+        switch (currentState)
+        {
+            case FSMStates.Idle:
+                UpdateIdleState();
+                break;
+            case FSMStates.Chase:
+                UpdateChaseState();
+                break;
+            case FSMStates.Attack:
+                UpdateAttackState();
+                break;
+            case FSMStates.Dead:
+                UpdateDeadState();
+                break;
         }
 
         elapsedTime += Time.deltaTime;
 
-        if (health <= 0) {
-            if(gameObject.CompareTag("Boss"))
+        if (health <= 0)
+        {
+            if (gameObject.CompareTag("Boss"))
             {
                 LevelManager.instance.PlayerWon();
             }
@@ -88,94 +92,141 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-void UpdateIdleState() {
-
-
-if (disToPlayer > Attackdistance) {
-        anim.SetTrigger("movementAnimationTrigger");
-        currentState = FSMStates.Chase;
+    void UpdateIdleState()
+    {
+        if (disToPlayer > Attackdistance && IsPlayerInClearPOV())
+        {
+            agent.isStopped = false;
+            zombieAI.SetIdleAnimation(false);
+            zombieAI.SetMovementAnimationTrigger();
+            agent.SetDestination(player.position);
+            currentState = FSMStates.Chase;
+        }
     }
-}
 
+    void UpdateChaseState()
+    {
+        FaceTarget(player.transform.position);
+        if (disToPlayer <= Attackdistance)
+        {
+            agent.isStopped = true;
+            zombieAI.SetAttackAnimation(true);
+            zombieAI.SetMovementAnimationTrigger();
+            currentState = FSMStates.Attack;
+        }
+        else if(!IsPlayerInClearPOV())
+        {
+            agent.isStopped = true;
+            zombieAI.SetIdleAnimation(true);
+            zombieAI.SetMovementAnimationTrigger();
+            currentState = FSMStates.Idle;
+        }
+        else if (timer % 10f == 0 && gameObject.CompareTag("Boss") && disToPlayer > Attackdistance)
+        {
+            agent.isStopped = true;
+            zombieAI.SetAttackAnimation(true);
+            zombieAI.SetMovementAnimationTrigger();
+            currentState = FSMStates.Attack;
+        }
+    }
 
-
-void UpdateChaseState() {
-    FaceTarget(player.transform.position);
-    if (disToPlayer <= Attackdistance) {
-       anim.ResetTrigger("movementAnimationTrigger");
-       anim.SetBool("attackPlayer", true);
-       currentState = FSMStates.Attack;
-    } else if(timer % 10f == 0 && gameObject.CompareTag("Boss")  && disToPlayer > Attackdistance) {
-       anim.ResetTrigger("movementAnimationTrigger");
-       anim.SetBool("attackPlayer", true);
-       currentState = FSMStates.Attack;
-       } else {
-        anim.SetBool("attackPlayer", false);
-}
-}
-
-void UpdateAttackState() {
-    FaceTarget(player.transform.position);
-    if(gameObject.CompareTag("Boss") && disToPlayer > Attackdistance) {
-        GameObject attack = Instantiate(throwable, 
-        spawnpoint.transform.position + transform.forward, transform.rotation) as GameObject;
+    void UpdateAttackState()
+    {
+        FaceTarget(player.transform.position);
+        if (gameObject.CompareTag("Boss") && disToPlayer > Attackdistance)
+        {
+            GameObject attack = Instantiate(throwable,
+                spawnpoint.transform.position + transform.forward, transform.rotation) as GameObject;
             Vector3 directionToPlayer = (player.position - spawnpoint.transform.position).normalized;
             Rigidbody rb = attack.GetComponent<Rigidbody>();
             rb.AddForce(directionToPlayer * 200, ForceMode.VelocityChange);
 
             hasAttacked = true;
-    if(hasAttacked){
-        anim.SetTrigger("movementAnimationTrigger");
-        currentState = FSMStates.Chase;
-    }
-    }
-    if (disToPlayer > Attackdistance) {
-        anim.SetTrigger("movementAnimationTrigger");
-        currentState = FSMStates.Chase;
+            if (hasAttacked)
+            {
+                agent.isStopped = false;
+                zombieAI.SetMovementAnimationTrigger();
+                agent.SetDestination(player.position);
+                zombieAI.SetAttackAnimation(false);
+                currentState = FSMStates.Chase;
+
+            }
+        }
+        if (disToPlayer > Attackdistance)
+        {
+            agent.isStopped = false;
+            zombieAI.SetMovementAnimationTrigger();
+            agent.SetDestination(player.position);
+            zombieAI.SetAttackAnimation(false);
+            currentState = FSMStates.Chase;
+
+        } else if (!IsPlayerInClearPOV()) {
+            zombieAI.SetAttackAnimation(false);
+            zombieAI.SetIdleAnimation(true);
+            currentState = FSMStates.Idle;
+        }
+
+        EnemyDamage();
     }
 
-    EnemyDamage();
-}
-
-
-    void UpdateDeadState() {
-        anim.SetBool("0Health", true);
+    void UpdateDeadState()
+    {
+        zombieAI.SetZeroHealthAnimation();
         isDead = true;
-        deadTransform = gameObject.transform;
         Destroy(gameObject, 5);
     }
- 
 
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 directionToTarget = (target - transform.position).normalized;
+        directionToTarget.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
 
-    void FaceTarget(Vector3 target) {
-Vector3 directionToTarget = (target - transform.position).normalized;
-directionToTarget.y = 0;
-Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
-transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
     }
 
-    void EnemyDamage() {
-        if(!isDead) 
+    void EnemyDamage()
+    {
+        if (!isDead)
         {
-            if(gameObject.CompareTag("Boss")) {
- 
+            if (gameObject.CompareTag("Boss"))
+            {
+
             }
-            //make sure you loop the attack animation
-        if(elapsedTime >= damRate && currentState == FSMStates.Attack) {
-        Invoke("DoDamage", 0.3f);
-        elapsedTime = 0.0f;
+            if (elapsedTime >= damRate && currentState == FSMStates.Attack)
+            {
+                Invoke("DoDamage", 0.3f);
+                elapsedTime = 0.0f;
+            }
         }
     }
-    }
 
-    void DoDamage() {
+    void DoDamage()
+    {
         var playerHealth = player.GetComponent<PlayerHealth>();
         playerHealth.TakeDamage(damAmt);
     }
 
     void IncrementTimer()
-{
-    timer++; 
-}
+    {
+        timer++;
+    }
+
+        bool IsPlayerInClearPOV() {
+
+        RaycastHit hit;
+        Vector3 directToPlayer = player.transform.position - enemyEyes.position;
+        if(Vector3.Angle(directToPlayer, enemyEyes.forward) <= FOV) {
+            if(Physics.Raycast(enemyEyes.position, directToPlayer, out hit)) {
+                if(hit.collider.CompareTag("Player")) {
+                    print("player in sight");
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
+    } 
 
 }
